2372e258e86bd0dc2c87899c750b672c98b22a99
Gustin33 <164647775+Gustin33@users.noreply.github.com>
2024-07-19 18:45:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function handleResponse(response) {
     const responseText = document.getElementById('response-text');
     
     // Ocultar a página inicial
-    initialPage.style.display = 'none';
+    initialPage.classList.add('hidden');
     
     // Mostrar a página de resposta
-    responsePage.style.display = 'flex';
+    responsePage.classList.remove('hidden');
     
     // Ajustar a mensagem de resposta
     responseText.textContent = response === 'sim'
