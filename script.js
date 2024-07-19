2616ceaf930c095af208985e1ade5d2a5fa917e9
Gustin33 <164647775+Gustin33@users.noreply.github.com>
2024-07-19 18:12:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-function confirmChoice(choice) {
-    const messageContainer = document.getElementById('message-container');
-    const message = document.getElementById('message');
+function handleResponse(response) {
+    const responseContainer = document.getElementById('response-container');
+    const responseMessage = document.getElementById('response-message');
 
-    if (choice === 'sim') {
-        message.innerHTML = 'Uhul! Que felicidade! <br> <div id="hearts"></div>';
+    if (response === 'sim') {
+        responseMessage.innerHTML = 'Uhul! Você aceitou! 💖<br>Que felicidade!';
     } else {
-        message.innerHTML = 'Tudo bem, sem problemas. <br> Espero que possamos ser amigos!';
+        responseMessage.innerHTML = 'Tudo bem, sem problemas. <br> Podemos ser amigos!';
     }
 
     document.getElementById('container').style.display = 'none';
-    messageContainer.style.display = 'flex';
+    responseContainer.style.display = 'flex';
     createHearts();
 }
 
 function createHearts() {
-    const heartContainer = document.getElementById('hearts');
-    for (let i = 0; i < 20; i++) {
+    const heartsContainer = document.getElementById('hearts');
+    for (let i = 0; i < 30; i++) {
         const heart = document.createElement('div');
         heart.classList.add('heart');
         heart.style.left = `${Math.random() * 100}vw`;
         heart.style.top = `${Math.random() * 100}vh`;
         heart.style.animationDuration = `${Math.random() * 3 + 2}s`;
-        heartContainer.appendChild(heart);
+        heartsContainer.appendChild(heart);
     }
 }
