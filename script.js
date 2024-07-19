59f8d20e70fd81b145bf7e917bb9f3824bc062fa
Gustin33 <164647775+Gustin33@users.noreply.github.com>
2024-07-19 18:15:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ function handleResponse(response) {
     if (response === 'sim') {
         responseMessage.innerHTML = 'Uhul! Você aceitou! 💖<br>Que felicidade!';
     } else {
-        responseMessage.innerHTML = 'Tudo bem, sem problemas. <br> Podemos ser amigos!';
+        responseMessage.innerHTML = 'Tudo bem, sem problemas.<br>Podemos ser amigos!';
     }
 
     document.getElementById('container').style.display = 'none';
-    responseContainer.style.display = 'flex';
+    responseContainer.classList.remove('hidden');
     createHearts();
 }
 
 function createHearts() {
     const heartsContainer = document.getElementById('hearts');
+    heartsContainer.innerHTML = ''; // Clear any existing hearts
     for (let i = 0; i < 30; i++) {
         const heart = document.createElement('div');
         heart.classList.add('heart');
         heart.style.left = `${Math.random() * 100}vw`;
         heart.style.top = `${Math.random() * 100}vh`;
-        heart.style.animationDuration = `${Math.random() * 3 + 2}s`;
+        heart.style.animationDuration = `${Math.random() * 5 + 5}s`;
         heartsContainer.appendChild(heart);
     }
 }
